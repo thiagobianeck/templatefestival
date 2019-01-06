77f12d865a1e98843ebd4ce9bc226450d3641f8d
Thiago Bianeck <thiagobianeck@gmail.com>
2019-01-06 20:27:51 -0200

    Aplicando estilos ao contador de dias restantes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
     $('#data').countdown('2019/08/23', function (event) {
-        $(this).html(event.strftime('%D Dias, %H Horas, %M Minutos e %S Segundos'));
+        $(this).html(event.strftime('<span>%D</span> Dias, <span>%H</span> Horas, <span>%M</span> Minutos e <span>%S</span> Segundos'));
     });
 });
